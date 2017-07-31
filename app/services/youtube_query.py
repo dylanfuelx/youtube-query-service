8c042f7ff8828d5906889fce245ddf79b6428235
Fuelx Admin <fuelxadmin@Fuelxs-MacBook-Pro.local>
2017-07-31 14:11:27 -0700

    Update API key in youtube service file.

--- app/services/youtube_query.py
+++ app/services/youtube_query.py
@@ -2,7 +2,7 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 
-DEVELOPER_KEY = "AIzaSyCNvXPmaEOyQHy5T7Za1t5tJ_v54ktzAeA"
+DEVELOPER_KEY = "AIzaSyDoc2019UguyagSyPpaMKCu3fHLTjT6a4g"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
